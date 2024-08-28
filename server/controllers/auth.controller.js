533d7416853884f08e360cb44078a887f11068e8
Mohammed <dev.mohammedsh@gmail.com>
2024-08-28 07:50:34 +0530

    🎒 Add user login

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -83,8 +83,49 @@ export const signup = async (req, res) => {
 
 		await sendVerificationEmail(user.email, verificationToken)
 
-		res.status(201).json({
-			// 200 => something created successfully
+		res.status(200).json({
+			success: true,
+			message: "User Logged in successfully",
+			user: {
+				...user._doc,
+				password: undefined,
+			},
+		})
+	} catch (error) {
+		console.log("Error in sign up")
+		res.status(400).json({ success: false, message: error.message })
+	}
+}
+
+export const login = async (req, res) => {
+	const { email, password } = req.body
+
+	try {
+		const user = await User.findOne({ email })
+
+		if (!user) {
+			return res.status(400).json({
+				success: false,
+				message: "Invalid credentials",
+			})
+		}
+
+		const isPasswordValid = await bcrypt.compare(password, user.password)
+
+		if (!isPasswordValid) {
+			return res.status(400).json({
+				success: false,
+				message: "Invalid credentials",
+			})
+		}
+
+		// jwt
+		generateTokenAndSetCookie(res, user._id)
+
+		user.lastLogin = new Date()
+		await user.save()
+
+		res.status(200).json({
 			success: true,
 			message: "User created successfully",
 			user: {
@@ -93,11 +134,13 @@ export const signup = async (req, res) => {
 			},
 		})
 	} catch (error) {
-		res.status(400).json({ success: false, message: error.message })
+		console.log("Error in login")
+		res.status(400).json({
+			success: false,
+			message: error.message,
+		})
 	}
 }
-
-export const login = async (req, res) => {}
 
 export const logout = async (req, res) => {
 	res.clearCookie("token")
