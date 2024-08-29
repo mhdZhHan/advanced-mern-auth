b86bb86867eb5a2466030d426a905f750d5d1cf3
Mohammed <dev.mohammedsh@gmail.com>
2024-08-29 12:29:07 +0530

    ⚙️ Configure for production 🌏

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import path from "node:path"
 import dotenv from "dotenv"
 import express from "express"
 import cors from "cors"
@@ -17,11 +18,17 @@ app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 app.use(express.json()) // allow parse incoming requests with JSON payload
 app.use(cookieParser()) // allow parse incoming cookies
 
-app.get("/", (req, res) => {
-	res.send("Hello, World!")
-})
-
+// routes
 app.use("/api/auth", authRoute)
+
+const __dirname = path.resolve()
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/client/dist")))
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+	})
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
